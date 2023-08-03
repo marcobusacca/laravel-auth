05bd60a89c01a23a231a6169a61ab4e163950c4b
marcobusacca <marcobusacca01@gmail.com>
2023-08-03 17:43:44 +0200

    [FEAT] projects index page layout

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,5 +1,45 @@
 @extends('layouts.admin')
 
 @section('content')
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center mt-5">
+                <h1>I nostri Progetti</h1>
+            </div>
+            <div class="col-12 mt-5">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Date of Creation</th>
+                            <th>Tools</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($projects as $project)
+                            <tr>
+                                <td>{{ $project->title }}</td>
+                                <td>{{ $project->date_of_creation }}</td>
+                                <td>
+                                    <a href="#" class="btn btn-info">
+                                        <i class="fas fa-eye"></i>
+                                    </a>
+                                    <a href="#" class="btn btn-warning">
+                                        <i class="fas fa-edit"></i>
+                                    </a>
+                                    <form class="d-inline-block" action="#" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="btn btn-danger">
+                                            <i class="fas fa-trash"></i>
+                                        </button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 @endsection
