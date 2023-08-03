62561b227e10a2a624fecd52f21849fdda03a931
marcobusacca <marcobusacca01@gmail.com>
2023-08-03 17:24:27 +0200

    [FEAT] import faker in project-seeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Project;
+use Faker\Generator as Faker;
 
 class ProjectSeeder extends Seeder
 {
