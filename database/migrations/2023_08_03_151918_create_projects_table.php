3b4f7625e02b2f267b849fb4081c9c7b45c4c026
marcobusacca <marcobusacca01@gmail.com>
2023-08-03 17:22:54 +0200

    [FEAT] projects table migration

--- database/migrations/2023_08_03_151918_create_projects_table.php
+++ database/migrations/2023_08_03_151918_create_projects_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50);
+            $table->text('description');
+            $table->date('date-of-creation');
             $table->timestamps();
         });
     }
