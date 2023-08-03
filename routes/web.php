dd4e0979e645a2c5a02a94757e26803df54acc3f
marcobusacca <marcobusacca01@gmail.com>
2023-08-03 17:43:59 +0200

    [FEAT] import project-controller in web route

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Admin\DashboardController as DashboardController;
 
+use App\Http\Controllers\Admin\ProjectController as ProjectController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
