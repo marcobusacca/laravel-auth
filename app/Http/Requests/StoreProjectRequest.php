521bee26b94a74036161f7ae2fd204a7424e268b
marcobusacca <marcobusacca01@gmail.com>
2023-08-21 16:17:52 +0200

    [FEAT] store and update project request

--- app/Http/Requests/StoreProjectRequest.php
+++ app/Http/Requests/StoreProjectRequest.php
@@ -24,7 +24,21 @@ class StoreProjectRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'title' => 'required|max:50',
+            'description' => 'required',
+            'date_of_creation' => 'required',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'title.required' => 'Il titolo è obbligatorio',
+            'title.max' => 'Il titolo deve avere una lunghezza massima di :max caratteri',
+
+            'description.required' => 'La descrizione è obbligatoria',
+
+            'date_of_creation.required' => 'La data di creazione è obbligatoria',
         ];
     }
 }
